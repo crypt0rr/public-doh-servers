5fb84908d91e0bd7d5b8248a0a8ce7e40e51d235
crypt0rr <57799908+crypt0rr@users.noreply.github.com>
2021-08-01 13:28:03 +0200

    Small change to preserve previous results

--- dns-to-ip.sh
+++ dns-to-ip.sh
@@ -3,6 +3,6 @@ INPUT="dns.list"
 read -p "Enter DNS server to use: " SERVER
 while IFS= read -r line
 do
-  dig @$SERVER +short -t "a" "$line" | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" >> tmp_out
+  dig @$SERVER +short -t "a" "$line" | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" >> ipv4.list
 done < "$INPUT"
-sort -u tmp_out | sort -h > ipv4.list && rm tmp_out
+sort -u ipv4.list | sort -h >> ipv4.list.new & mv ipv4.list.new ipv4.list
