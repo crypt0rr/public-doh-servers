1dc67806f6fcef8643209b26e1eb0370754ca205
crypt0rr <57799908+crypt0rr@users.noreply.github.com>
2022-07-16 00:46:05 +0200

    Fix

--- dns-to-ip.sh
+++ dns-to-ip.sh
@@ -5,4 +5,4 @@ while IFS= read -r line
 do
   dig @$SERVER +short -t "a" "$line" | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" >> ipv4.list
 done < "$INPUT"
-sort -u ipv4.list | sort -h >> ipv4.list.new & mv ipv4.list.new ipv4.list
+sort -u ipv4.list | sort -h >> ipv4.list.new && mv ipv4.list.new ipv4.list
