4d19b92fd289315cd63dd905b17bf1583f1e5aa6
crypt0rr <57799908+crypt0rr@users.noreply.github.com>
2022-11-30 16:18:31 +0100

    Switched to argument parsing

--- dns-to-ip.sh
+++ dns-to-ip.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 INPUT="dns.list"
-read -p "Enter DNS server to use: " SERVER
 while IFS= read -r line
 do
-  dig @$SERVER +short -t "a" "$line" | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" >> ipv4.list
+  dig @$1 +short -t "a" "$line" | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" >> ipv4.list
 done < "$INPUT"
 sort -u ipv4.list | sort -h >> ipv4.list.new && mv ipv4.list.new ipv4.list
