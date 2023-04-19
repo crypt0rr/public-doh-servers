ed60c4f0a588f35f8d073541fd0acaef09bed219
Bart <57799908+crypt0rr@users.noreply.github.com>
2023-04-19 13:10:31 +0200

    IPv6 support (#3)
    * Update dns-to-ip.sh to build IPv6 list
    
    * Update IPv4
    
    * Update IPv6

--- dns-to-ip.sh
+++ dns-to-ip.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 INPUT="dns.list"
+
+# IPv4
 while IFS= read -r line
 do
   dig @$1 +short -t "a" "$line" | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" >> ipv4.list
 done < "$INPUT"
 sort -u ipv4.list | sort -h >> ipv4.list.new && mv ipv4.list.new ipv4.list
+
+# IPv6
+while IFS= read -r line
+do
+  dig @$1 +short -t "aaaa" "$line" | grep -oE "(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))" >> ipv6.list
+done < "$INPUT"
+sort -u ipv6.list | sort -h >> ipv6.list.new && mv ipv6.list.new ipv6.list
